72c9aed1d5b64eeb92d49898e5133c542c75316c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-04-16 18:30:57 +0900

    ラッパーの機能をビューから呼び出すテスト
    (1) ラッパーの提供する機能を VB から呼び出し

--- View/ViewVb/Form1.vb
+++ View/ViewVb/Form1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim message As String
+        Dim result As Integer
+        Dim objWrapper As Wrapper.Class1 = New Wrapper.Class1()
+
+        message = TextBox1.Text
+        result = objWrapper.test1(message)
+        MsgBox("入力した文字列の長さは " & result, MsgBoxStyle.OkOnly)
+    End Sub
 End Class
