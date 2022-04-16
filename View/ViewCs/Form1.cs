618b50d870e8167b0df42fb602b583c1afe9c813
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-04-16 18:48:06 +0900

    プロジェクトファイルを新規作成
    (1) ビュー用の C# フォーム

---
+++ View/ViewCs/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ViewCs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.String  message = textBox1.Text;
+            int result;
+            Wrapper.Class1 objWrapper = new Wrapper.Class1();
+
+            result = objWrapper.test1(message);
+            MessageBox.Show("長さは " + result);
+        }
+    }
+}
