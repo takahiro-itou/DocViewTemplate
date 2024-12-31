b8786f3259074b511da55a7246abf92366fccbfe
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-12-31 13:32:06 +0900

    ユーザーコントロールにプロパティを実装

--- View/WinFormsControl/SampleControl.cs
+++ View/WinFormsControl/SampleControl.cs
@@ -6,5 +6,23 @@
         {
             InitializeComponent();
         }
+
+        //----------------------------------------------------------------
+        //    InputText プロパティ
+        //
+        public String InputText
+        {
+            get { return txtInput.Text; }
+            set { txtInput.Text = value; }
+        }
+
+        //----------------------------------------------------------------
+        //    OutputText プロパティ
+        //
+        public String OutputText
+        {
+            get { return txtOutput.Text; }
+            set { txtOutput.Text = value; }
+        }
     }
 }
