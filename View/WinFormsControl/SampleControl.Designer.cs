92e2339ada3e96877b5e86f601bfd1b7d69d1030
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-12-31 12:42:29 +0900

    ユーザーコントロールをデザイン

--- View/WinFormsControl/SampleControl.Designer.cs
+++ View/WinFormsControl/SampleControl.Designer.cs
@@ -28,11 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SampleControl));
+            this.Label1 = new Label();
+            this.txtInput = new TextBox();
+            this.Label2 = new Label();
+            this.txtOutput = new TextBox();
+            this.btnRun = new Button();
+            this.SuspendLayout();
+            //
+            // Label1
+            //
+            resources.ApplyResources(Label1, "Label1");
+            Label1.Name = "Label1";
+            //
+            // txtInput
+            //
+            resources.ApplyResources(txtInput, "txtInput");
+            txtInput.Name = "txtInput";
+            //
+            // Label2
+            resources.ApplyResources(Label2, "Label2");
+            Label2.Name = "Label2";
+            //
+            // txtOutput
+            //
+            resources.ApplyResources(txtOutput, "txtOutput");
+            txtOutput.Name = "txtOutput";
+            txtOutput.ReadOnly = true;
+            //
+            // btnRun
+            //
+            resources.ApplyResources(btnRun, "btnRun");
+            btnRun.Name = "btnRun";
+            btnRun.UseVisualStyleBackColor = true;
+            //
+            // SampleControl
+            //
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(Label1);
+            this.Controls.Add(txtInput);
+            this.Controls.Add(Label2);
+            this.Controls.Add(txtOutput);
+            this.Controls.Add(btnRun);
+            resources.ApplyResources(this, "$this");
+            this.Name = "SampleControl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private Label Label1;
+        private TextBox txtInput;
+        private Label Label2;
+        private TextBox txtOutput;
+        private Button btnRun;
     }
 }
