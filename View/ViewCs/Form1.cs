d6ea235dca85c4756141129ed3373298bb4f6d8f
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-01-01 14:21:27 +0900

    イベントハンドラを実装

--- View/ViewCs/Form1.cs
+++ View/ViewCs/Form1.cs
@@ -1,23 +1,35 @@
 ﻿
 namespace ViewCs
 {
-    public partial class Form1 : Form
+
+public partial class Form1 : Form
+{
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            System.String  message = TextBox1.Text;
-            int result;
-            SampleWrapper.Common.SampleDocument objWrapper = new SampleWrapper.Common.SampleDocument();
-
-            objWrapper.setMessage(message);
-            result = objWrapper.countAlphabet();
-            MessageBox.Show("文字列中のアルファベットの個数は " + result);
-        }
+        InitializeComponent();
     }
+
+    private void runCount(System.String message)
+    {
+        int result;
+        SampleWrapper.Common.SampleDocument objWrapper = new SampleWrapper.Common.SampleDocument();
+
+        objWrapper.setMessage(message);
+        result = objWrapper.countAlphabet();
+        System.String outText = "文字列中のアルファベットの個数は " + result;
+        SampleControl1.OutputText = outText;
+        MessageBox.Show(outText);
+    }
+
+    private void Button1_Click(object sender, EventArgs e)
+    {
+        runCount(SampleControl1.InputText);
+    }
+
+    private void SampleControl1_RunButtonClick(object sender, EventArgs e)
+    {
+        runCount(SampleControl1.InputText);
+    }
+}
 
 }
