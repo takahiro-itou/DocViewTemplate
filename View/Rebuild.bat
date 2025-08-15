b5d5d6809b6f270b4021b3189df48710d91875c4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-08-15 13:29:32 +0900

    リビルド用のバッチファイルを分割

--- View/Rebuild.bat
+++ View/Rebuild.bat
@@ -1,3 +1,2 @@
 
 msbuild -restore -t:Rebuild -p:Configuration="Release" -p:Platform=x64  SampleView.sln
-msbuild -restore -t:Rebuild -p:Configuration="Release" -p:Platform=x64  SampleView.Net48.sln
