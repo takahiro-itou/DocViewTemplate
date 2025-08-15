c3f0a871c3058a7bd91fc9b7a6eb5eb308a5d45e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-08-15 13:15:48 +0900

    リビルド用のバッチファイルを修正

--- View/Rebuild.bat
+++ View/Rebuild.bat
@@ -1,2 +1,3 @@
 
-msbuild -restore -t:Rebuild -p:Configuration="Release" -p:Platform=x64
+msbuild -restore -t:Rebuild -p:Configuration="Release" -p:Platform=x64  SampleView.sln
+msbuild -restore -t:Rebuild -p:Configuration="Release" -p:Platform=x64  SampleView.Net48.sln
